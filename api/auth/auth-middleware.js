4fdb65350f8e0ca7ccf4d16cb09de8b283c5b60e
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2022-04-04 14:01:27 -0600

    ok i got some endpoints fleshed out with some middleware helpers -rough draft

---
+++ api/auth/auth-middleware.js
@@ -0,0 +1,76 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../secrets"); // use this secret!
+const User = require("../users/model");
+
+const restricted = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decodedJwt) => {
+      if (err) {
+        next({ status: 401, message: "Token invalid" });
+      } else {
+        req.decodedJwt = decodedJwt;
+        next();
+      }
+    });
+  } else {
+    next({ status: 401, message: "Token required" });
+  }
+};
+
+const only = (role_name) => (req, res, next) => {
+  if (req.decodedJwt && req.decodedJwt.role_name === role_name) {
+    next();
+  } else {
+    next({ status: 403, message: "Authorized personnel only" });
+  }
+};
+
+const checkUsernameExists = async (req, res, next) => {
+  try {
+    let user = await User.findBy({ username: req.body.username });
+
+    if (!user) {
+      next({ status: 401, message: "Invalid credentials" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+async function checkUsernameFree(req, res, next) {
+  let [user] = await User.findBy({ username: req.body.username });
+  if (user != null) {
+    next({ status: 422, message: "Username taken" });
+  } else {
+    next();
+  }
+}
+
+const validateRoleName = (req, res, next) => {
+  let user = req.body;
+
+  if (!user.role_name || user.role_name.trim() == "") {
+    user.role_name = "client";
+  }
+  if (user.role_name.trim() === "instructor") {
+    res.status(422).json({ message: "Role name can not be instructor" });
+  } else if (user.role_name.trim().length > 32) {
+    res.status(422).json({
+      message: "Role name can not be longer than 32 characters",
+    });
+  } else {
+    req.role_name = user.role_name.trim();
+    next();
+  }
+};
+
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  validateRoleName,
+  only,
+};
---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const clientRouter = require("./auth/auth-router.js");
+const instructorRouter = require("./users/instructor-router.js");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("/api/client", clientRouter);
+server.use("/api/instructor", instructorRouter);
+
+server.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
