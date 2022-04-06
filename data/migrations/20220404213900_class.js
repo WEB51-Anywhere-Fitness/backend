3a765de25d2d01c28be9cbce746f6ba4ea20e80a
Will Phillips <42WPhillips42@gmail.com>
2022-04-05 20:56:43 -0500

    migrations and seed files completed, only a thing or two left to fix

--- data/migrations/20220404213900_class.js
+++ data/migrations/20220404213900_class.js
@@ -1,23 +1,41 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("classes", (tbl) => {
-    tbl.increments("class_id");
-    tbl.string("name", 128).notNullable().unique();
-    tbl.string("type", 128).notNullable();
-    tbl.string("start_time", 128).notNullable();
-    tbl.string("duration", 128).notNullable();
-    tbl.string("intensity_level", 128).notNullable();
-    tbl.string("location", 128).notNullable();
-    tbl.integer("registered_attendees", 32).notNullable();
-    tbl.integer("max_class_size", 32).notNullable();
-    tbl
-      .integer("role_id")
+  return knex.schema
+    .createTable("classes", (tbl) => {
+      tbl.increments("class_id");
+      tbl.string("name", 128).notNullable().unique();
+      tbl.string("type", 128).notNullable();
+      tbl.string("start_time", 128).notNullable();
+      tbl.string("duration", 128).notNullable();
+      tbl.string("intensity_level", 128).notNullable();
+      tbl.string("location", 128).notNullable();
+      tbl.integer("registered_attendees", 32).notNullable();
+      tbl.integer("max_class_size", 32).notNullable();
+      tbl
+        .integer("instructor_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onUpdate("RESTRICT")
+        .onDelete("RESTRICT");
+    })
+    .createTable('class_clients', (tbl) => {
+      tbl.increments('class_client_id');
+      tbl.integer('class_id')
+        .unsigned()
+        .notNullable()
+        .references('class_id')
+        .inTable('classes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      tbl.integer('user_id')
       .unsigned()
       .notNullable()
-      .references("role_id")
-      .inTable("roles")
-      .onUpdate("RESTRICT")
-      .onDelete("RESTRICT");
-  });
+      .references('user_id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    })
 };
 
 exports.down = function (knex) {
